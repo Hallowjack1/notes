bf110170cc88f67d7c9a8043c84ecc6c9834d811
Hallowjack1 <matthewty94@gmail.com>
2026-05-22 16:29:40 +0800

    update CORS for production

--- backend/api/trash.php
+++ backend/api/trash.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: https://notes-psi-murex.vercel.app/');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: POST, GET');
 header('Content-Type: application/json');
